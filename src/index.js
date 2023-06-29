5d19b6efa65a990919f83205d3d5c4109896a378
Noé Gambini <noe.gambini@ornikar.com>
2023-06-29 10:18:08 +0200

    chore: update dependencies

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import AppWeb from './App.web';
 import AppNative from './App.native';
 
 // For React technical test, uncomment this next line
-const App = AppWeb;
+// const App = AppWeb;
 
 // For React-native technical test, uncomment this next line
-// const App = AppNative;
+const App = AppNative;
 
 ReactDOM.render(
   <KittThemeProvider>
